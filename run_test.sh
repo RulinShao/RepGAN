802810d1bbe0c511b73bfc6146d0cdeff84b691d
RulinShao <srl0310@outlook.com>
2022-04-28 15:11:04 -0400

    Training pipline

--- run_test.sh
+++ run_test.sh
@@ -2,4 +2,4 @@
 
 METFACES=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
 
-python test.py --network_pkl $METFACES
+python test.py --network_pkl $METFACES --batch_size 64
